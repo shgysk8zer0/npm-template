bb494a0c6b31ffbd232c6845beabb9fc9fdfbfa1
Chris Zuber <shgysk8zer0@gmail.com>
2023-10-03 10:40:43 -0700

    Update Rollup config and `exports`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Changed
+- Setup to transpile all `./*.js` to `./cjs/*.cjs` (except `*.config.js`)
+- Update `exports` and `main` accordingly
+
 ## [v1.1.1] - 2023-09-24
 
 ### Added
--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
-import { getConfig } from '@shgysk8zer0/js-utils/rollup';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { warningHandler } from '@shgysk8zer0/js-utils/rollup';
+import { listDirByExt } from '@shgysk8zer0/npm-utils/fs';
 
-export default getConfig('./index.js', {
-	format: 'cjs',
-	minify: false,
-	sourcemap: false,
+const modules = await listDirByExt('./', '.js');
+
+export default {
+	input: modules.filter(module => ! module.endsWith('.config.js')),
 	external: [],
-	plugins: [],
-	globals: {},
-});
+	onwarn: warningHandler,
+	output: {
+		dir: './cjs/',
+		format: 'cjs',
+		preserveModules: true,
+		entryFileNames: '[name].cjs',
+		chunkFileNames: '[name]-[hash].cjs',
+	},
+	plugins: [nodeResolve()],
+};
